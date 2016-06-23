47a6d7b0cb06e1530461fede5c1c97e4a8ed6629
root <root@docker1.skgbolo.local>
2016-06-23 15:26:30 -0400

    more changes to compose

--- scripts/docker-rm.sh
+++ scripts/docker-rm.sh
@@ -1,6 +1,6 @@
 # remove all hyperledger peer containers
-docker stop $(docker ps -a --format='{{.Names}}' --filter "name=hyperledger_vp*")
-docker rm $(docker ps -a --format='{{.Names}}' --filter "name=hyperledger_vp*")
+docker stop $(docker ps -a --format='{{.Names}}' --filter "name=vp*")
+docker rm $(docker ps -a --format='{{.Names}}' --filter "name=vp*")
 # remove all hyperledger chaincode containers
 docker stop $(docker ps -a --format='{{.Names}}' --filter "name=dev-vp*")
 docker rm $(docker ps -a --format='{{.Names}}' --filter "name=dev-vp*")
